c51bf948d5b40bd7de37f5ccb7930e8691e64959
jawaharjayakumar <54067860+jawaharjayakumar@users.noreply.github.com>
2019-08-15 20:49:45 +0530

    Create alphabet.cpp

---
+++ alphabet.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cin>>c;
+    if(isalpha(c))
+    {
+        cout<<"Alphabet";
+    }
+    else
+    {
+        cout<<"No";
+    }
+}
