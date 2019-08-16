2d68b7ccebbbeda6c31f146dacd38d33c7b3ab72
jawaharjayakumar <54067860+jawaharjayakumar@users.noreply.github.com>
2019-08-16 13:04:08 +0530

    Update oddinterval.cpp

--- oddinterval.cpp
+++ oddinterval.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main()
 {
-    int N,K;
-    cin>>N>>K;
-    for(int i=N+1;i<K;i++)
+    int N,Q;
+    cin>>N>>Q;
+    for(int i=N+1;i<Q;i++)
     {
         if(i%2==!0)
         {
